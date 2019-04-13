d3d6ffeb9b8d15042f6820ab03e56c27da886789
sensoumya123 <46414050+sensoumya123@users.noreply.github.com>
2019-04-14 00:00:31 +0530

    Delete commits.sh

--- commits.sh
+++
@@ -1,5 +0,0 @@
-# Git: add and commit changes
-cd /root/project1 && /usr/bin/git add --all && /usr/bin/git commit -m "daily crontab backup `date`"
-
-# send data to Git server
-cd /root/project1 && /usr/bin/git push origin master
